90146286b22411679ad6ceda4de7e2e8362331e4
Samudra Perera <samudrapup@gmail.com>
2022-09-20 03:36:55 -0400

    Fizz Buzz

--- easy/happyNumber.js
+++ easy/happyNumber.js
@@ -10,11 +10,14 @@
 const isHappy = function(n) {
     const seen = new Set()
     while(n !== 1) {
+        console.log(seen)
         let currentSum = 0;
         while(n > 0) {
-            currentSum += (n % 10) * (n % 10)
-            n = Math.floor(n/10)
+            console.log(currentSum)
+            currentSum += (n % 10) * (n % 10) // (n%2) returns the last digit of n only
+            n = Math.floor(n/10) //Decrements by base 100 until n is a decimal and when floored it is == 0
         }
+
         if(!seen.has(currentSum)) {
             seen.add(currentSum)
             n = currentSum
@@ -24,3 +27,5 @@ const isHappy = function(n) {
     }
     return true
 };
+
+isHappy(19)
