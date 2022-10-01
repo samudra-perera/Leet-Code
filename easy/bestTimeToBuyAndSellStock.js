e3b17c20c69c9c988d475816f56bfb518bcea803
Samudra Perera <samudrapup@gmail.com>
2022-09-30 21:01:21 -0400

    Modified 121 and started 225

--- easy/bestTimeToBuyAndSellStock.js
+++ easy/bestTimeToBuyAndSellStock.js
@@ -19,23 +19,21 @@
 // } This is the brute force solution Time complexity is o(n^2)
 
 const maxProfit = (prices) => {
-    let profit = 0
-    let buyPrice = prices[0]
-    for(let i = 0; i < prices.length-1; i++) {
-        let tempProfit = prices[i+1] - prices[i]
-        if(tempProfit > 0) {
-            if(prices[i] < buyPrice) {
-                buyPrice = prices[i]
+    let profit = 0               //Init profit with profit var
+    let buyPrice = prices[0]    //Holds the current buy value
+    for(let i = 0; i < prices.length-1; i++) {  //Iterate through the array
+        let tempProfit = prices[i+1] - prices[i]    //if index+1 subtract the current index 
+        if(tempProfit > 0) {                        //if tempProfit is greater than 0 that means that the prices[index+1] is larger than prices[index] meaning if you buy at prices
+            if(prices[i] < buyPrice) {              //meaning if you buy at prices[index] and sell at prices[index+1] you will make a profit
+                buyPrice = prices[i]                //check if the price[index] is less than the current buy price, if it is, make the new buy price the current index
             }
-            
-            if(prices[i+1] - buyPrice > profit) {
-                profit = prices[i+1] - buyPrice
+
+            if(prices[i+1] - buyPrice > profit) {  //after reassigning the new buyPrice check if the profit made is greater than the prev profit
+                profit = prices[i+1] - buyPrice     //make profit the next index subtract the current buyPrice
             }
         }
-        
     }
     return profit
 } 
-
 
 
