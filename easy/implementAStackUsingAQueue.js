ff7d987028cc2c03cee1f2a89dc570067b31480f
Samudra Perera <samudrapup@gmail.com>
2022-10-01 00:37:39 -0400

    ImplementAStackUsingAQueue

--- easy/implementAStackUsingAQueue.js
+++ easy/implementAStackUsingAQueue.js
@@ -13,7 +13,7 @@
 // Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
 
 class Node {
-    constructor() {
+    constructor(val) {
         this.val = val;
         this.next = null
     }
@@ -27,6 +27,65 @@ class MyQueue {
     }
 
     enqueue(val) {
+        const newNode = new Node(val)
+
+        if(!this.size) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.size++
+    }
+
+    dequeue() {
+        if(!this.size) return null
         
+        let node = this.head
+        this.head = this.head.next
+        node.next = null
+        this.size--
+
+        return node.val
+
+    }
+
+    peek() {
+        return this.head.val
+    }
+
+    empty() {
+        return this.size === 0
+    }
+}
+
+class MyStack {
+    constructor() {
+        this.queue = new MyQueue()
+
+    }
+
+    push(val) {
+        let rotate = this.queue.size; 
+        this.queue.enqueue(val)
+
+        while(rotate) {
+            this.queue.enqueue(this.queue.dequeue())
+            rotate--
+        }
+
+    }
+
+    pop() {
+        return this.queue.dequeue()
+    }
+
+    top() {
+        return this.queue.peek()
+    }
+
+    empty() {
+        return this.queue.empty()
     }
 }
